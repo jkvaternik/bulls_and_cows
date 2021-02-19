c4f2ed3068c4e0b8d8fb516b3c5f6efbac32f253
Jaime <jaime.kvaternik17@gmail.com>
2021-02-19 14:44:44 -0500

    Edit server config files

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -11,12 +11,7 @@ secret_key_base =
     You can generate one by calling: mix phx.gen.secret
     """
 
-config :bulls_and_cows, BullsAndCowsWeb.Endpoint,
-  http: [
-    port: String.to_integer(System.get_env("PORT") || "4000"),
-    transport_options: [socket_opts: [:inet6]]
-  ],
-  secret_key_base: secret_key_base
+config :bulls_and_cows, BullsAndCowsWeb.Endpoint, server: true
 
 # ## Using releases (Elixir v1.9+)
 #
