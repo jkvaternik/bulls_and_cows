3591f0514dd8f2730bd22632ff316de5b1c9fb63
Jaime <jaime.kvaternik17@gmail.com>
2021-02-19 15:47:54 -0500

    Update config

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,12 +4,12 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-secret_key_base = "fwsBaa89AiQovQbcoEn9Q16nGEGEJ0/6pYIce7MlxCRsj+X1Mwod/rijz6Xl12ay"
-  # System.get_env("SECRET_KEY_BASE") ||
-  #   raise """
-  #   environment variable SECRET_KEY_BASE is missing.
-  #   You can generate one by calling: mix phx.gen.secret
-  #   """
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
 
 config :bulls_and_cows, BullsAndCowsWeb.Endpoint, server: true
 
